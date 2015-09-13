1786e591f9d02bcdc60ef190a570ea9966ceb7d6
planoie@gmail.com <planoie@gmail.com>
2015-09-13 00:42:11 -0400

    Add package action remind notice.

--- src/Pelasoft.AspNet.Mvc.Slack/nuget-pack.bat
+++ src/Pelasoft.AspNet.Mvc.Slack/nuget-pack.bat
@@ -1,1 +1,11 @@
+@echo off
+echo.
+echo ######  Nuget Creation Checklist  ######
+echo ##                                    ##
+echo ##  * Update assembly version number  ##
+echo ##  * Update nuspec revision notes    ##
+echo ##  * Create tag after final commit   ##
+echo ##                                    ##
+echo ########################################
+echo.
 nuget pack Pelasoft.AspNet.Mvc.Slack.csproj -Verbosity detailed -Build -IncludeReferencedProjects -Properties Configuration=Release
